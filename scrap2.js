70a465a37c258993b086080d22e8ec81c856e71e
LiboBearChen <50467944+LiboBearChen@users.noreply.github.com>
2020-01-15 12:02:30 -0500

    Update scrap2.js

--- scrap2.js
+++ scrap2.js
@@ -3,8 +3,6 @@ const cheerio = require("cheerio");
 
 request('http://libogit.azurewebsites.net',(error,response,html)=>{
     if(!error && response.statusCode == 200){
-        const $ = cheerio.load(html);
-
-        
+        const $ = cheerio.load(html);      
     }
 });
